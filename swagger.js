abc669ae45a80703e565eefdeb4848c937c47f64
Kathryn Lambert <113531714+kathrynruth8@users.noreply.github.com>
2025-05-31 15:25:02 -0600

    back to localhost

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Soda Recipe API',
     description: 'API for managing custom soda recipes',
   },
-  host: 'cse341-project-d23b.onrender.com',
-  schemes: ['https'],
+  host: 'localhost:3000',
+  schemes: ['http'],
 };
 
 const outputFile = './swagger.json';
