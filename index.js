cc08b83c730b9eb3760bd2f74b3ae36db63c0eff
Kathryn Lambert <113531714+kathrynruth8@users.noreply.github.com>
2025-05-31 21:25:04 -0600

    fix render

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const swaggerFile = require('./swagger.json');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
