2eed9d82e2a3e93addd6950a1c8c07cb93e48150
Kathryn Lambert <113531714+kathrynruth8@users.noreply.github.com>
2025-06-14 17:39:30 -0600

    add swagger for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-
+// Get all users
 router.get('/', userController.getAllUsers);
+
+// Get user by ID
+router.get('/:id', userController.getUserById);
+
+// Add new user
 router.post('/', userController.createUser);
+
+// update user
+router.put(':/id', userController.updateUser);
+
+// Delete user
+router.delete(':/id', userController.deleteUser);
 
 module.exports = router;
